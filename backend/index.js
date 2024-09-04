67da3bd83cea011d4f7071e6eec6f7ff5df88765
Nischit17 <nischit819@gmail.com>
2024-09-05 00:23:22 +0530

    Updated Folder Structure and Created routes, contollers, middlewares for user and Tested Register, Login, Update Profile and Logout in postman

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config({});
 
 const app = express();
 
 // Middleware
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
@@ -21,6 +23,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+// API'S
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
